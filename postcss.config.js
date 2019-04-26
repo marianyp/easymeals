9a5d3dbbec1eef02ddf2e23ae7cd7df91ecfb736
Mariany Pinkston <marianypinkston@gmail.com>
2019-04-26 10:45:31 -0400

    Change configs for netlify instead of github pages

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,22 @@
 module.exports = {
   plugins: [
     require("tailwindcss")("tailwind.js"),
-    require("autoprefixer")(),
     //Only add purgecss in production
     process.env.NODE_ENV === "production"
       ? require("@fullhuman/postcss-purgecss")({
-          content: ["./src/**/*.html", "./src/**/*.vue"]
+          content: ["./src/**/*.html", "./src/**/*.vue"],
+          extractors: [
+            {
+              extractor: class TailwindExtractor {
+                static extract(content) {
+                  return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+                }
+              },
+              extensions: ["css", "html", "vue"]
+            }
+          ]
         })
-      : ""
+      : "",
+    require("autoprefixer")
   ]
 };
